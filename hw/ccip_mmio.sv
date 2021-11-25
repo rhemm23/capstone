47e232cb88bdf1134e4454bbb0b225673af633cf
rhemm23 <ryanhemmila@gmail.com>
2021-11-25 03:12:13 -0600

    Comments

--- hw/ccip_mmio.sv
+++ hw/ccip_mmio.sv
@@ -63,24 +63,33 @@ module ccip_mmio
 
   always_ff @(posedge clk or posedge rst) begin
     if (rst) begin
-
-      // Channel 0
       tx.c0.hdr <= 0;
       tx.c0.valid <= 0;
-
-      // Channel 1
       tx.c1.hdr <= 0;
       tx.c1.valid <= 0;
-
-      // Channel 2
       tx.c2.hdr <= 0;
       tx.c2.mmioRdValid <= 0;
-
     end else begin
+
+      /*
+       * MMIO read request
+       */
       if (rx.c0.mmioRdValid) begin
+
+        /*
+         * Echo TID
+         */
         tx.c2.mmioRdValid <= 1'b1;
         tx.c2.hdr.tid <= mmio_hdr.tid;
+
+        /*
+         * Set response data
+         */
         case (mmio_hdr.address)
+
+          /*
+           * AFU Header
+           */
           16'h0000: tx.c2.data <= {
             4'b0001, // Feature type = AFU
             8'b0,    // reserved
@@ -91,10 +100,26 @@ module ccip_mmio
             4'b0,    // afu major revision = 0
             12'b0    // feature ID = 0
           };
+
+          /*
+           * AFU ID lower
+           */
           16'h0002: tx.c2.data <= afu_id[63:0];
+
+          /*
+           * AFU ID higher
+           */
           16'h0004: tx.c2.data <= afu_id[127:64];
+
+          /*
+           * Reserved
+           */
           16'h0006: tx.c2.data <= 64'h0;
           16'h0008: tx.c2.data <= 64'h0;
+
+          /*
+           * Unknown
+           */
           default:  tx.c2.data <= 64'h0;
         endcase
       end else begin
