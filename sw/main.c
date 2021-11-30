9d093fb03f81d050c6dbf6b3bf9a72a9a742501b
Ryan Hemmila <ryanhemmila@gmail.com>
2021-11-29 19:27:56 -0600

    Working on command line arguments

--- sw/main.c
+++ sw/main.c
@@ -1,10 +1,41 @@
 #include <inttypes.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 #include "afu.h"
 #include "afu_json_info.h"
 
-int main() {
+static void exit_with_error(char *error) {
+  fprintf(stderr, "Error: %s\n", error);
+  exit(EXIT_FAILURE);
+}
+
+int main(int argc, char *argv[]) {
+
+  /*
+   * Progress command line args
+   */
+  char *program_path;
+
+  for (int i = 1; i < argc; i++) {
+    if (strcmp("--help", argv[i]) == 0 || strcmp("-h", argv[i]) == 0) {
+      printf("Command line arguments:\n\n");
+      printf("\t<--help, -h>                      -> Print command line args\n");
+      printf("\t<--program, -p> <path to program> -> CPU program to execute\n");
+      exit(0);
+    }
+    if (strcmp("--program", argv[i]) == 0 || strcmp("-p", argv[i]) == 0) {
+      if (i == argc - 1) {
+        exit_with_error("Expected program path after option");
+      } else {
+        program_path = argv[++i];
+      }
+    }
+  }
+  if (program_path == NULL) {
+    exit_with_error("Missing program path parameter, use --help or -h");
+  }
 
   afu_t afu;
 
