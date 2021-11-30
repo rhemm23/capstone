83be4057f2593a0def036660c41ce1ff07df365b
Ryan Hemmila <ryanhemmila@gmail.com>
2021-11-30 05:44:48 -0600

    api auth done

--- api/app.py
+++ api/app.py
@@ -1,11 +1,7 @@
-from bson.objectid import ObjectId
 from pymongo import MongoClient
-from functools import wraps
+from account import account
 
-import datetime
-import bcrypt
 import flask
-import jwt
 import os
 
 client = MongoClient()
@@ -14,53 +10,16 @@ db = client.capstone
 app = flask.Flask(__name__)
 app.secret_key = os.urandom(24).hex()
 
-def authenticate(route):
-  @wraps(route)
-  def decorated(*args, **kwargs):
-    auth_header = flask.request.headers.get('authorization', None)
-    if auth_header:
-      parts = auth_header.split()
-      if len(parts) == 2 and parts[0].lower() == 'bearer':
-        try:
-          token = jwt.decode(parts[1], app.secret_key, algorithms='HS256')
-          account_id = ObjectId(token['account_id'])
-          if db.accounts.count_documents({ '_id': account_id }, limit=1) == 1:
-            return route(account_id, *args, **kwargs)
-        except jwt.exceptions.InvalidTokenError:
-          pass
-    return api_error('Missing or invalid authorization token')
-  return decorated
+app.register_blueprint(account)
 
 def api_success(**kwargs):
-  result = { 'success': True }
-  for key, value in kwargs.items():
-    result[key] = value
-  return flask.jsonify(result), 200
+  if kwargs:
+    return flask.jsonify(kwargs), 200
+  else:
+    return '', 204
 
 def api_error(error):
-  result = {
-    'success': False,
-    'error': error
-  }
-  return flask.jsonify(result), 400
-
-@app.route('/api/test')
-@authenticate
-def test(id):
-  return api_success(id=str(id))
-
-@app.route('/api/accounts', methods=['POST'])
-def create_account():
-  username = flask.request.form.get('username', None)
-  password = flask.request.form.get('password', None)
-  if not username or not password:
-    return api_error('Missing username or password')
-  if db.accounts.count_documents({ 'username': username }, limit=1) != 0:
-    return api_error('Username is already in use')
-  password_hash = bcrypt.hashpw(str.encode(password), bcrypt.gensalt())
-  account_doc = { 'username': username, 'password_hash': password_hash }
-  account_id = db.accounts.insert_one(account_doc).inserted_id
-  return api_success(token=jwt.encode({ 'account_id': str(account_id) }, app.secret_key))
+  return flask.jsonify({ 'error': error }), 400
 
 if __name__ == '__main__':
-  app.run(port=5000)
+  app.run()
