abb6ba0b32d0441711062819a9e9b3b531041bbf
Ryan Hemmila <ryanhemmila@gmail.com>
2021-12-02 15:29:32 -0600

    added luts for iru

---
+++ gen/gen_iru_sin_lut.py
@@ -0,0 +1,29 @@
+import math
+
+with open('../hw/iru/iru_sin_lut.sv', 'w+') as lut:
+  lut.write('module iru_sin_lut\n')
+  lut.write('  (\n')
+  lut.write('    /*\n')
+  lut.write('     * Inputs\n')
+  lut.write('     */\n')
+  lut.write('    input [5:0] d,\n\n')
+  lut.write('    /*\n')
+  lut.write('     * Outputs\n')
+  lut.write('     */\n')
+  lut.write('    output reg [8:0] q\n')
+  lut.write('  );\n\n')
+  lut.write('  always_comb begin\n')
+  lut.write('    case (d)\n')
+
+  for i in range(36):
+    init = format(i, 'b').zfill(6)
+    value = math.sin(math.radians(i * 10))
+    sign = '1' if value < 0 else '0'
+    res = format(int(128 * abs(value)), 'b').zfill(8)
+
+    lut.write('      9\'b{0}: q <= 8\'b{1}{2};\n'.format(init, sign, res))
+
+  lut.write('      default: q <= 0;\n')
+  lut.write('    endcase\n')
+  lut.write('  end\n\n')
+  lut.write('endmodule\n')
