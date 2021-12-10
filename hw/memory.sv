00aec31648ee483bdc6b8bb7e9b8ae9ff1aebd44
rhemm23 <ryanhemmila@gmail.com>
2021-12-10 00:52:14 -0600

    memory write

--- hw/memory.sv
+++ hw/memory.sv
@@ -8,8 +8,10 @@ module memory
      */
     input clk,
     input rst_n,
-    input request_valid,
+    input read_request_valid,
+    input write_request_valid,
     input [31:0] address,
+    input [511:0] data_d,
     input t_if_ccip_Rx rx,
 
     /*
@@ -17,19 +19,23 @@ module memory
      */
     output buffer_addr_valid,
     output data_valid,
-    output [511:0] data,
+    output write_done,
+    output [511:0] data_q,
     output t_if_ccip_Tx tx
   );
 
-  typedef enum logic [1:0] {
-    IDLE = 2'b00,
-    WAIT = 2'b01,
-    SENT = 2'b10
+  typedef enum logic [2:0] {
+    IDLE = 3'b000,
+    READ_REQUEST = 3'b001,
+    WRITE_REQUEST = 3'b010,
+    WAIT_READ_RESPONSE = 3'b011,
+    WAIT_WRITE_RESPONSE = 3'b100
   } mem_state;
 
   mem_state state;
 
   reg [31:0] stored_address;
+  reg [511:0] stored_data;
 
   wire [63:0] buffer_addr;
 
@@ -44,18 +50,23 @@ module memory
   always_ff @(posedge clk or negedge rst_n) begin
     if (!rst_n) begin
       stored_address <= '0;
+      stored_data <= '0;
       state <= IDLE;
       tx.c0 <= '0;
       tx.c1 <= '0;
     end else begin
       case (state)
         IDLE: begin
-          if (request_valid) begin
+          if (read_request_valid) begin
             stored_address <= address;
-            state <= WAIT;
+            state <= READ_REQUEST;
+          end else if (write_request_valid) begin
+            stored_address <= address;
+            stored_data <= data_d;
+            state <= WRITE_REQUEST;
           end
         end
-        WAIT: if (!rx.c0TxAlmFull) begin
+        READ_REQUEST: if (!rx.c0TxAlmFull) begin
           tx.c0.hdr <= '{
             eVC_VA,
             2'b00,
@@ -66,22 +77,49 @@ module memory
             16'h0000
           };
           tx.c0.valid <= 1;
-          state <= SENT;
+          state <= WAIT_READ_RESPONSE;
         end
-        SENT: begin
+        WAIT_READ_RESPONSE: begin
           if (rx.c0.rspValid && rx.c0.hdr.resp_type == eRSP_RDLINE && rx.c0.hdr.mdata == 16'h0000) begin
             state <= IDLE;
           end
           tx.c0.valid <= 0;
         end
+        WRITE_REQUEST: if (!rx.c1TxAlmFull) begin
+          tx.c1.hdr <= '{
+            0,
+            eVC_VA,
+            1'b1,
+            eMOD_CL,
+            eCL_LEN_1,
+            eREQ_WRLINE_I,
+            0,
+            t_ccip_clAddr'(buffer_addr + stored_address),
+            16'h0000
+          };
+          tx.c1.valid <= 1;
+          tx.c1.data <= stored_data;
+          state <= WAIT_WRITE_RESPONSE;
+        end
+        WAIT_WRITE_RESPONSE: begin
+          if (rx.c1.rspValid && rx.c1.hdr.resp_type == eRSP_WRLINE && rx.c1.hdr.mdata == 16'h0000) begin begin
+            state <= IDLE;
+          end
+          tx.c1.valid <= 0;
+        end
       endcase
     end
   end
 
+  assign write_done = (rx.c1.rspValid) &&
+                      (rx.c1.hdr.resp_type == eRSP_WRLINE) &&
+                      (rx.c1.hdr.mdata == 16'h0000) &&
+                      (state == WAIT_WRITE_RESPONSE);
+
   assign data_valid = (rx.c0.rspValid) &&
                       (rx.c0.hdr.resp_type == eRSP_RDLINE) &&
                       (rx.c0.hdr.mdata == 16'h0000) &&
-                      (state == SENT);
+                      (state == WAIT_READ_RESPONSE);
 
   assign data = rx.c0.data;
   assign buffer_addr_valid = |buffer_addr;
