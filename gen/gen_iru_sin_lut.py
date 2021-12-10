2e341c7e8bd85dc642c2353d91b1c652dcaad14b
rhemm23 <ryanhemmila@gmail.com>
2021-12-09 23:45:05 -0600

    iru working

--- gen/gen_iru_sin_lut.py
+++ gen/gen_iru_sin_lut.py
@@ -2,6 +2,8 @@ from bitstring import Bits
 
 import math
 
+NUM_BITS = 32
+
 with open('../hw/iru/iru_sin_lut.sv', 'w+') as lut:
   lut.write('module iru_sin_lut\n')
   lut.write('  (\n')
@@ -12,7 +14,7 @@ with open('../hw/iru/iru_sin_lut.sv', 'w+') as lut:
   lut.write('    /*\n')
   lut.write('     * Outputs\n')
   lut.write('     */\n')
-  lut.write('    output reg [8:0] q\n')
+  lut.write('    output reg [{0}:0] q\n'.format(NUM_BITS - 1))
   lut.write('  );\n\n')
   lut.write('  always_comb begin\n')
   lut.write('    case (d)\n')
@@ -21,10 +23,10 @@ with open('../hw/iru/iru_sin_lut.sv', 'w+') as lut:
     init = ['0' for _ in range(36)]
     init[i] = '1'
     value = math.sin(math.radians(i * 10))
-    res = Bits(int=int(128 * value), length=9)
-    lut.write('      36\'b{0}: q <= 8\'b{1};\n'.format(''.join(init)[::-1], res.bin))
+    res = Bits(int=int(2**(NUM_BITS - 2) * value), length=NUM_BITS)
+    lut.write('      36\'b{0}: q <= {1}\'b{2};\n'.format(''.join(init)[::-1], NUM_BITS, res.bin))
 
-  lut.write('      default: q <= 0;\n')
+  lut.write('      default: q <= \'0;\n')
   lut.write('    endcase\n')
   lut.write('  end\n\n')
   lut.write('endmodule\n')
--- hw/iru/iru_comp_unit.sv
+++ hw/iru/iru_comp_unit.sv
@@ -15,11 +15,20 @@ module iru_comp_unit
     output [4:0] col_q
   );
 
+  wire signed [37:0] row_m_cos;
+  wire signed [37:0] row_m_sin;
+
+  wire signed [37:0] col_m_cos;
+  wire signed [37:0] col_m_sin;
+
   wire signed [15:0] calc_row;
   wire signed [15:0] calc_col;
 
-  wire signed [8:0] cos_q;
-  wire signed [8:0] sin_q;
+  wire signed [31:0] cos_q;
+  wire signed [31:0] sin_q;
+
+  wire signed [5:0] row_shift;
+  wire signed [5:0] col_shift;
 
   iru_cos_lut cos_lut (
     .d(rnn_out),
@@ -31,8 +40,17 @@ module iru_comp_unit
     .q(sin_q)
   );
 
-  assign calc_col = (((($signed({ 1'b0, col_d }) - 10) * cos_q) - (($signed({ 1'b0, row_d }) - 10) * sin_q)) >>> 7) + 10;
-  assign calc_row = (((($signed({ 1'b0, col_d }) - 10) * sin_q) + (($signed({ 1'b0, row_d }) - 10) * cos_q)) >>> 7) + 10;
+  assign row_shift = $signed({ 1'b0, row_d }) - 10;
+  assign col_shift = $signed({ 1'b0, col_d }) - 10;
+
+  assign row_m_cos = (row_shift * cos_q) >>> 30;
+  assign row_m_sin = (row_shift * sin_q) >>> 30;
+
+  assign col_m_cos = (col_shift * cos_q) >>> 30;
+  assign col_m_sin = (col_shift * sin_q) >>> 30;
+
+  assign calc_col = (col_m_cos - row_m_sin) + 10;
+  assign calc_row = (col_m_sin + row_m_cos) + 10;
 
   assign col_q = calc_col[4:0];
   assign row_q = calc_row[4:0];
