65d5b20c95639b8cb3839021c8c1d5cc37bda4ca
rhemm23 <ryanhemmila@gmail.com>
2021-12-10 03:25:27 -0600

    added write test

--- hw/ctrl_unit.sv
+++ hw/ctrl_unit.sv
@@ -23,8 +23,10 @@ module ctrl_unit
     WAIT_BUFFER = 3'b000,
     FETCH_PROGRAM_PAGE = 3'b001,
     WAIT_PROGRAM_PAGE = 3'b010,
-    EXECUTING = 3'b011,
-    DONE = 3'b100
+    WRITE_PROGRAM_PAGE = 3'b011,
+    WAIT_WRITE_PAGE = 3'b100,
+    EXECUTING = 3'b101,
+    DONE = 3'b110
   } ctrl_unit_state;
 
   ctrl_unit_state state;
@@ -51,7 +53,8 @@ module ctrl_unit
         end
         WAIT_PROGRAM_PAGE: if (data_valid) begin
           if (cnt == 255) begin
-            state <= EXECUTING;
+            state <= WRITE_PROGRAM_PAGE;
+            cnt <= 0;
           end else begin
             state <= FETCH_PROGRAM_PAGE;
             cnt <= cnt + 1;
@@ -60,12 +63,23 @@ module ctrl_unit
             instructions[(cnt * 16) + i] <= read_data[(i * 32) +: 32];
           end
         end
-        EXECUTING: begin
-          /* TODO */
+        WRITE_PROGRAM_PAGE: begin
+          state <= WAIT_WRITE_PAGE;
+        end
+        WAIT_WRITE_PAGE: if (write_done) begin
+          if (cnt == 255) begin
+            state <= DONE;
+          end else begin
+            state <= WRITE_PROGRAM_PAGE;
+            cnt <= cnt + 1;
+          end
         end
       endcase
     end
   end
+
+  assign write_request_valid = (state == WRITE_PROGRAM_PAGE);
+  assign write_data = { 512 { 1'b1 } };
 
   assign address = cnt;
   assign write_request_valid = 0;
