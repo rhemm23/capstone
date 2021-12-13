f13bf175b486a94dc3e2c85f3da453a77a802994
Dvorak17 <wyatt.f.dvorak@gmail.com>
2021-12-13 12:21:24 -0600

    Testing synth on devcloud for floating point

--- hw/afu.sv
+++ hw/afu.sv
@@ -54,18 +54,4 @@ module afu
     .write_request_valid(write_request_valid)
   );
 
-  rot_nn rnn (
-    .clk(clk),
-    .rst_n(rst_n),
-    .start(),
-    .data_in(),
-    .write_weight(),
-    .layer_sel(),
-    .neuron_sel(),
-    .weight_sel(),
-    .weight_bus(),
-    .angle(),
-    .done()
-  );
-
 endmodule
