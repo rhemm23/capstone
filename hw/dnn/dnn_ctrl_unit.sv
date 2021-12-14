61ae57e34f0399c24765100d55711aa4eccf976c
Ryan Hemmila <ryanhemmila@gmail.com>
2021-12-14 01:37:02 -0600

    dnn ctrl unit should be working

--- hw/dnn/dnn_ctrl_unit.sv
+++ hw/dnn/dnn_ctrl_unit.sv
@@ -19,18 +19,20 @@ module dnn_ctrl_unit
     output reg [511:0] results
   );
 
-  typedef enum logic [] {
-    IDLE,
-    WAIT_NN,
-    DONE
+  typedef enum logic [1:0] {
+    IDLE = 2'b00,
+    WAIT_NN = 2'b01,
+    DONE = 2'b10
   } dnn_state;
 
   dnn_state state;
 
+  reg [1:0] page_cnt;
   reg [10:0] cnt;
 
   always_ff @(posedge clk or negedge rst_n) begin
     if (!rst_n) begin
+      page_cnt <= '0;
       results <= '0;
       state <= IDLE;
       cnt <= '0;
@@ -39,7 +41,23 @@ module dnn_ctrl_unit
         state <= WAIT_NN;
       end
       WAIT_NN: if (det_nn_done) begin
-        
+        results[cnt] <= det_nn_result;
+        if ((page_cnt < 2) && (cnt + 1 == 512)) begin
+          page_cnt <= page_cnt + 1;
+          state <= DONE;
+          cnt <= '0;
+        end else if ((page_cnt == 2) && (cnt + 1 == 62)) begin
+          page_cnt <= '0;
+          state <= DONE;
+          cnt <= '0;
+        end else begin
+          cnt <= cnt + 1;
+          state <= IDLE;
+        end
+      end
+      DONE: if (next_in_ready) begin
+        results <= '0;
+        state <= IDLE;
       end
     end
   end
@@ -47,6 +65,6 @@ module dnn_ctrl_unit
   assign in_ready = (state == IDLE);
   assign start_det_nn = (state == IDLE) & prev_out_ready;
 
-  assign out_ready = (state == );
+  assign out_ready = (state == DONE);
 
 endmodule
