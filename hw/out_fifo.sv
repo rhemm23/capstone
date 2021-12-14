009cf007fad73de23d8136a78b3aab1b08a47779
sjain75 <sjain75@wisc.edu>
2021-12-13 23:06:38 -0600

    ctrl ipgu interaction

--- hw/out_fifo.sv
+++ hw/out_fifo.sv
@@ -1,4 +1,4 @@
-module out_fifo  #(Q_DEPTH = 80) 
+module out_fifo  #(DATA_WIDTH=8, Q_DEPTH = 80) 
   (
     /*
      * Inputs
@@ -6,17 +6,17 @@ module out_fifo  #(Q_DEPTH = 80)
     input clk,
     input rst_n,
     input en,
-    input [7:0] d,
+    input [DATA_WIDTH-1:0] d,
 
     /*
      * Outputs
      */
-    output reg [7:0] q [Q_DEPTH-1:0]
+    output reg [DATA_WIDTH-1:0] q [Q_DEPTH-1:0]
   );
 
   always @(posedge clk or negedge rst_n) begin
     if (!rst_n) begin
-      q <= '{ Q_DEPTH { 8'h00 } };
+      q <= '{ Q_DEPTH { {DATA_WIDTH{1'b0}} } };
     end else if (en) begin
       q[0] <= d;
       q[Q_DEPTH-1:1] <= q[Q_DEPTH-2:0];
