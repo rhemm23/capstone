c5d2b1f5acee767d947fcaf7937c6bbef86e9957
Dvorak17 <48167089+Dvorak17@users.noreply.github.com>
2021-12-18 23:12:35 -0600

    Clean code

--- hw/area_eff_modules/control_wrapper.sv
+++ hw/area_eff_modules/control_wrapper.sv
@@ -51,13 +51,8 @@ module control_wrapper
     wire reg_wr_en;
     wire [27:0] reg_databus;
     wire begin_proc;    
-    /*
-    Interconnection needed
-        input incPc,                                  
-        input instrVld,                               
-        input [31:0] instructionsIn [NUM_INSTR-1:0];         
-        output [31:0] instr;                    
-    */
+ 
+    
     instruction_fetch  instructionFetch (
         .clk(clk),
         .rst_n(rst_n),
@@ -71,19 +66,7 @@ module control_wrapper
 
 
      
-    /*
-    Interconncetion needed
-        //instruction_fetch -> Decode        
-        input   [31:0]  instr,               
-        //ctrl_unit (registers) <- Decode    
-        output  [1:0]   reg_sel,             
-        output          wr_en,               
-        output  [27:0]  reg_databus,         
-        //ctrl_unit (cmds) <- Decode         
-        output          begin_rdn_load,      
-        output          begin_dnn_load,      
-        output          begin_proc           
-    */
+
     decode decode(
         .clk(clk),
         .rst_n(rst_n),
@@ -98,63 +81,7 @@ module control_wrapper
     );
 
 
-    //ctrl_unit
-        
-    //!!instructions = instructionsIn!!
-    /*
-    Interconncetion needed
-        //ctrl_unit <-> instructionFetch
-        output  reg       incPc,                //DONE
-        output  [31:0]    instructions [NUM_INSTR-1:0],
-        output  reg       instrVld,             //DONE
-    */
-
-    //!!reg_wr_en=wr_en!!
-    /*
-    Interconncetion needed
-        //decode -> ctrl_unit (cmds)
-        input           begin_rdn_load,
-        input           begin_dnn_load,
-        input           begin_proc,     
-        //decode -> ctrl_unit (registers)
-        input   [1:0]   reg_sel,
-        input           reg_wr_en,
-        input   [27:0]  reg_databus,         
-    */
-
-    //defined as is out of the wrapper 
-    /*
-    Interconncetion needed
-        //ctrl_unit <-> memory
-        input buffer_addr_valid,
-        input data_valid,
-        input write_done,
-        input [511:0] read_data,
-        output reg [31:0] address,
-        output [511:0] write_data,
-        output reg read_request_valid,
-
-        //IPGU <-> ctrlUnit
-        output    reg     wrAll,
-        output   [8-1:0]  wrAllData [300-1:0][300-1:0],
-        output   reg      initIpgu,
-        input             rdyIpgu,
-
-        //rdn <-> ctrl_unit
-        //weights
-        input           rdnReqWeightMem,
-        input           doneWeightRdn,
-        output  [63:0]  rdn_weights [7:0],
-
-        //dnn <-> ctrl_unit
-        input           dnnResVld,
-        input   [1085:0]dnnResults,
-        output  reg     dnnResRdy,
-        //weights
-        input           dnnReqWeightMem,
-        input           doneWeightDnn,
-        output  [63:0]  dnn_weights [7:0]
-    */
+    
     
     ctrl_unit2  ctrlUnit(
         .clk(clk),
